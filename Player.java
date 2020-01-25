4757a671c3c8381faad42ac7620db315b36b067d
Drew Smith <smithdrew555@yahoo.com>
2020-01-25 17:06:22 -0500

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,11 @@
+import java.awt.image.BufferedImage;
+
+public class Player extends CollideableSprite {
+    public Player(){
+        super();
+    }
+    public Player(BufferedImage image, int x, int y){
+        super(image, x, y);
+    }
+
+}
